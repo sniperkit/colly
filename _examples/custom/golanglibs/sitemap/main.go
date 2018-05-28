e9b3e7093c5d0cb8c83b97384d13148fcaa78a8d
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-05-28 19:36:38 +0200

    clean sitemap example

---
+++ _examples/custom/golanglibs/sitemap/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sniperkit/colly/pkg"
+)
+
+func main() {
+
+	// Array containing all the known URLs in a sitemap
+	knownUrls := []string{}
+
+	// Create a Collector specifically for Shopify
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.golanglibs.com"),
+	)
+
+	// Create a callback on the XPath query searching for the URLs
+	c.OnXML("//urlset/url/loc", func(e *colly.XMLElement) {
+		knownUrls = append(knownUrls, e.Text)
+	})
+
+	// Start the collector
+	c.Visit("https://www.golanglibs.com/sitemap.xml")
+
+	fmt.Println("All known URLs:")
+	for _, url := range knownUrls {
+		fmt.Println("\t", url)
+	}
+
+	fmt.Println("Collected", len(knownUrls), "URLs")
+
+}
