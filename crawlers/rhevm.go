919391b558948264cc59d083be95260fca8744d3
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-05-30 11:09:30 +0200

    Squashed '_projects/resource-crawler/' content from commit 05669ae
    git-subtree-dir: _projects/resource-crawler
    git-subtree-split: 05669ae1e52a1ab4ca7bc581184d69b4f2dbb27c

---
+++ crawlers/rhevm.go
@@ -0,0 +1,78 @@
+package crawlers
+
+import (
+	"bytes"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"regexp"
+	"time"
+
+	"github.com/gocolly/colly"
+	log "github.com/sirupsen/logrus"
+)
+
+// RhevmBuild is
+type RhevmBuild struct {
+	BuildName  string `bson:"_id"`
+	URLs       []string
+	Downloaded bool
+}
+
+// RhevmBuildCrawler is
+type RhevmBuildCrawler struct {
+	Conf
+	RpmURL string
+}
+
+// ColName is
+func (r RhevmBuildCrawler) ColName() string {
+	return r.CollectionName
+}
+
+func (r RhevmBuildCrawler) parse(data []byte) (buildName string, ready bool) {
+	ready = bytes.Contains(data, []byte("Ready"))
+	if !ready {
+		log.Debugf("Not ready, %s", data)
+		return buildName, ready
+	}
+	log.Debugf("Build is Ready: %s", data)
+	pattern := regexp.MustCompile(`\[rhv-.+\]`)
+	ret := pattern.Find(data)
+
+	return fmt.Sprintf("%s", bytes.Trim(ret, "[]")), ready
+}
+
+// Crawl is
+func (r RhevmBuildCrawler) Crawl() (result interface{}, err error) {
+	log.Infof("crawl %s is starting", r.CrawlerName)
+	var res []RhevmBuild
+
+	c := colly.NewCollector(
+		colly.AllowedDomains(r.AllowDomains),
+		// colly.CacheDir(fmt.Sprintf("/tmp/%s_cache", r.CrawlerName)),
+	)
+	c.SetRequestTimeout(time.Second * 30)
+	c.WithTransport(&http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	})
+
+	c.OnResponse(func(req *colly.Response) {
+		if name, ready := r.parse(req.Body); ready {
+			rhevmBuild := RhevmBuild{
+				BuildName:  name,
+				URLs:       []string{r.RpmURL},
+				Downloaded: false,
+			}
+			res = append(res, rhevmBuild)
+		}
+	})
+
+	err = c.Visit(r.StartURL)
+	if err != nil {
+		log.Error(err)
+		return result, err
+	}
+
+	return res, nil
+}
