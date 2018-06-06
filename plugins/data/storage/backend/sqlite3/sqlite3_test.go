5d168b7732adf7052ddac2bba538cea755a7de2d
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-06-06 17:31:27 +0200

    add colly storage: redis & sqlite3

--- plugins/data/storage/backend/sqlite3/sqlite3_test.go
+++ plugins/data/storage/backend/sqlite3/sqlite3_test.go
@@ -1,6 +1,8 @@
 package sqlite3
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSqlite3Storage_Visited(t *testing.T) {
 	s := &Storage{
@@ -14,15 +16,15 @@ func TestSqlite3Storage_Visited(t *testing.T) {
 
 	//defer s.Clear()
 
-	ids := []uint64{1,2,3,4,5}
+	ids := []uint64{1, 2, 3, 4, 5}
 
-	for _, u := range ids{
+	for _, u := range ids {
 		if err := s.Visited(u); err != nil {
 			t.Error("failed to add visit: " + err.Error())
 		}
 	}
 
-	for _, u := range ids{
+	for _, u := range ids {
 		if _, err := s.IsVisited(u); err != nil {
 			t.Error("failed to check visit: " + err.Error())
 		}
@@ -42,14 +44,14 @@ func TestSqlite3Storage_AddRequest(t *testing.T) {
 
 	//defer s.Clear()
 
-	request1 := []byte{1,2,3,4,5,6}
-	request2 := []byte{7,8,9,10,11,12}
+	request1 := []byte{1, 2, 3, 4, 5, 6}
+	request2 := []byte{7, 8, 9, 10, 11, 12}
 
-	if err := s.AddRequest(request1); err !=nil {
+	if err := s.AddRequest(request1); err != nil {
 		t.Error("failed to AddRequest" + err.Error())
 	}
 
-	if err := s.AddRequest(request2); err !=nil {
+	if err := s.AddRequest(request2); err != nil {
 		t.Error("failed to AddRequest" + err.Error())
 	}
 
@@ -67,21 +69,21 @@ func TestSqlite3Storage_QueueSize(t *testing.T) {
 
 	//defer s.Clear()
 
-	request1 := []byte{1,2,3,4,5,6}
-	request2 := []byte{7,8,9,10,11,12}
+	request1 := []byte{1, 2, 3, 4, 5, 6}
+	request2 := []byte{7, 8, 9, 10, 11, 12}
 
-	if err := s.AddRequest(request1); err !=nil {
+	if err := s.AddRequest(request1); err != nil {
 		t.Error("failed to AddRequest" + err.Error())
 	}
 
-	if err := s.AddRequest(request2); err !=nil {
+	if err := s.AddRequest(request2); err != nil {
 		t.Error("failed to AddRequest" + err.Error())
 	}
 
-	if size, err := s.QueueSize(); err !=nil {
+	if size, err := s.QueueSize(); err != nil {
 		t.Error("failed to get queue size: " + err.Error())
 		return
-	} else if size !=2 {
+	} else if size != 2 {
 		t.Errorf("queue size is not correct should be 2 but is: %v", size)
 	} else {
 		t.Log("queue size is correct")
@@ -102,20 +104,22 @@ func TestSqlite3Storage_GetRequest(t *testing.T) {
 
 	//defer s.Clear()
 
-	request1 := []byte{1,2,3,4,5,6}
-	request2 := []byte{7,8,9,10,11,12}
+	request1 := []byte{1, 2, 3, 4, 5, 6}
+	request2 := []byte{7, 8, 9, 10, 11, 12}
 
-	if err := s.AddRequest(request1); err !=nil {
+	if err := s.AddRequest(request1); err != nil {
 		t.Error("failed to AddRequest" + err.Error())
 	}
 
-	if err := s.AddRequest(request2); err !=nil {
+	if err := s.AddRequest(request2); err != nil {
 		t.Error("failed to AddRequest" + err.Error())
 	}
 
 	for {
 		b, err := s.GetRequest()
-		if b == nil {break}
+		if b == nil {
+			break
+		}
 		t.Logf("b: %v", b)
 		if err != nil {
 			t.Error("failed to GetRequest: " + err.Error())
@@ -124,7 +128,7 @@ func TestSqlite3Storage_GetRequest(t *testing.T) {
 
 		count += 1
 
-		if count > 2{
+		if count > 2 {
 			t.Error("failed to get 2 requests; got more")
 			break
 		}
