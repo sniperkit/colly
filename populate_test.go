bc8d943a2daba19a439e166eb0f4ed001ccc4abd
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-06-10 11:49:56 +0200

    Squashed 'plugins/data/structure/reflect/reflectutils/v1/' content from commit 2d491b47
    git-subtree-dir: plugins/data/structure/reflect/reflectutils/v1
    git-subtree-split: 2d491b4785ecdd3c9880bc0b7c640ca4cc954d19

---
+++ populate_test.go
@@ -0,0 +1,14 @@
+package reflectutils
+
+// import (
+// 	"testing"
+// )
+
+// func TestPopulate(t *testing.T) {
+// 	var v1 *Person
+// 	Populate(&v1)
+// 	if v1.Company == nil {
+// 		t.Errorf("not populated %+v", v1)
+// 	}
+
+// }
