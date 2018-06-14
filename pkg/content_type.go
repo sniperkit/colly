69610b503c6513a3f24ea9a8f10f14b135de0b7e
Rosco Pecoltran <rosco_pecoltran@msn.com>
2018-06-14 14:55:58 +0200

    prepare decoders for tabular and sitemaps

--- pkg/content_type.go
+++ pkg/content_type.go
@@ -7,3 +7,20 @@ type HandleOnType interface {
 
 // handleOnType function...
 func (c *Collector) handleOnType(resp *Response) error { return ErrNotImplementedYet }
+
+/*
+func (s *Collector) initEncodingDefaults() {
+	switch strings.ToLower(collector.Encoding) {
+	case TAB:
+		collector.Decoder = NewTABDecoder(false)
+	case HTML:
+		collector.Decoder = NewHTMLDecoder(false)
+	case XML:
+		collector.Decoder = NewXMLDecoder(false)
+	case RSS:
+		collector.Decoder = NewRSSDecoder(false)
+	default:
+		collector.Decoder = NewJSONDecoder(false)
+	}
+}
+*/
